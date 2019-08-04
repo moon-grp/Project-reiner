d50f7bc1727617467d4cc04d8dcb87d12b983355
moon-grp <olumidemm@gmail.com>
2019-08-04 09:09:30 +0100

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+     <v-container  fluid>
+
+       <v-layout mt-12 align-center justify-space-around  wrap>
+
+          <v-flex   xs12 md4 ma-1>
+           <div class="text-f">
+            <v-text-field
+            rounded
+            v-model="eng_word"
+            label="Solo"
+            placeholder="Enter English Text"
+            solo
+          ></v-text-field>
+           </div>
+            
+            <v-radio-group v-model="language" row>
+      <v-radio label="Russian" value="ru"></v-radio>
+      <v-radio label="French" value="fr"></v-radio>
+      <v-radio label="Latin" value="la"></v-radio>
+        <v-radio label="Chinese" value="zh"></v-radio>
+    </v-radio-group>
+          
+          <div class="text-center">
+          <v-btn rounded
+           color="success"
+           @click="translate"
+           >Translate</v-btn>
+          </div>
+
+      </v-flex>
+    
+      <v-flex xs12 md4 ma-1>
+        <v-card dark class="text-d" height="200px" flat>
+           <div class="text-center">
+          <v-card-text >Translation</v-card-text>
+          <v-card-text class="display-2" >{{translation}}</v-card-text>
+           </div>
+        </v-card>
+      </v-flex>
+
+       </v-layout>
+     </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+
+  },
+  data: () => ({
+    language: "ru",
+    eng_word: "",
+    translation:""
+  }),
+  methods:{
+    translate(){
+      try {
+         axios.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190804T065058Z.dc1f9e38631388bb.104ca214624880a09302466c7c9a7da5116c79d6&lang=${this.language}&text=${this.eng_word}`)
+       .then(res=>{
+         console.log(res.data.text[0])
+         this.translation=res.data.text[0]
+       })
+      } catch (error) {
+        console.log(error)
+      }
+      
+      
+    }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.text-f{
+  margin-top: 150px
+}
+.text-d{
+  margin-top: 100px
 }
 </style>
